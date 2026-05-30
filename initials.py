c7f8fc1972983865a2cb0687fee8323797473ff0
Ofori Deborah Appiah <2425401182@live.gctu.edu.gh>
2026-05-30 17:42:19 -0100

    Submit initials assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
 
-def main():
-  pass #code goes here
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: Ofori Deborah Appiah
+# STUDENT ID: 2425401182
+# Email: 2425401182@live.gctu.edu.gh
+# Grader: Deborah
+#
+# On my honor, Ofori Deborah Appiah, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+
+def main():
+    # Print initials ODA in stylized large block letters
+
+    print()
+    print("...ODA")
+    print()
+    print("....OOOOOOOOOO.........DDDDDDDD................AA...........")
+    print("...OO........OO........DDDDDDDDDD.............AAAA..........")
+    print("...OO........OO........DD.......DDD..........AA...AA........")
+    print("...OO........OO........DD........DD.........AA.....AA.......")
+    print("...OO........OO........DD........DD.........AA......AA......")
+    print("...OO........OO........DD........DD........AA.......AA......")
+    print("...OO........OO........DD........DD........AAAAAAAAAAAA.....")
+    print("...OO........OO........DD.......DDD........AA........AA.....")
+    print("...OO........OO...**...DDDDDDDDDD.....**...AA........AA...**")
+    print("....OOOOOOOOOO....**...DDDDDDDD.......**...AA........AA...**")
+    print()
 
 
 main()
